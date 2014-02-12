14f885225dc727c144bedbc7990ab229f2934320
David M. Persson <davidpersson@gmx.de>
2014-02-12 22:21:41 +0100

    Use controller traits.

--- controllers/PostsController.php
+++ controllers/PostsController.php
@@ -12,100 +12,15 @@
 
 namespace cms_post\controllers;
 
-use cms_post\models\Posts;
-use lithium\g11n\Message;
-use li3_flash_message\extensions\storage\FlashMessage;
+class PostsController extends \cms_core\controllers\BaseController {
 
-class PostsController extends \lithium\action\Controller {
+	use \cms_core\controllers\AdminIndexTrait;
+	use \cms_core\controllers\AdminAddTrait;
+	use \cms_core\controllers\AdminEditTrait;
+	use \cms_core\controllers\AdminDeleteTrait;
 
-	public function admin_index() {
-		$data = Posts::find('all', ['with' => 'CoverMedia']);
-		return compact('data');
-	}
-
-	public function admin_add() {
-		extract(Message::aliases());
-
-		$item = Posts::create();
-
-		if ($this->request->data) {
-			if ($item->save($this->request->data)) {
-				FlashMessage::write($t('Successfully saved.'));
-				return $this->redirect(['action' => 'index', 'library' => 'cms_post']);
-			} else {
-				FlashMessage::write($t('Failed to save.'));
-			}
-		}
-		$this->_render['template'] = 'admin_form';
-		return compact('item');
-	}
-
-	public function admin_edit() {
-		extract(Message::aliases());
-
-		$item = Posts::find($this->request->id);
-
-		if ($this->request->data) {
-			if ($item->save($this->request->data)) {
-				FlashMessage::write($t('Successfully saved.'));
-				return $this->redirect(['action' => 'index', 'library' => 'cms_post']);
-			} else {
-				FlashMessage::write($t('Failed to save.'));
-			}
-		}
-		$this->_render['template'] = 'admin_form';
-		return compact('item');
-	}
-
-	public function admin_delete() {
-		extract(Message::aliases());
-
-		$item = Posts::find($this->request->id);
-		$item->delete();
-		FlashMessage::write($t('Successfully deleted.'));
-
-		return $this->redirect(['action' => 'index', 'library' => 'cms_post']);
-	}
-
-	public function admin_publish() {
-		extract(Message::aliases());
-
-		$item = Posts::find($this->request->id);
-		$item->save(['is_published' => true]);
-		FlashMessage::write($t('Successfully published.'));
-
-		return $this->redirect(['action' => 'index', 'library' => 'cms_post']);
-	}
-
-	public function admin_unpublish() {
-		extract(Message::aliases());
-
-		$item = Posts::find($this->request->id);
-		$item->save(['is_published' => false]);
-		FlashMessage::write($t('Successfully unpublished.'));
-
-		return $this->redirect(['action' => 'index', 'library' => 'cms_post']);
-	}
-
-	public function admin_promote() {
-		extract(Message::aliases());
-
-		$item = Posts::find($this->request->id);
-		$item->save(['is_promoted' => true]);
-		FlashMessage::write($t('Successfully promoted.'));
-
-		return $this->redirect(['action' => 'index', 'library' => 'cms_post']);
-	}
-
-	public function admin_unpromote() {
-		extract(Message::aliases());
-
-		$item = Posts::find($this->request->id);
-		$item->save(['is_promoted' => false]);
-		FlashMessage::write($t('Successfully unpromoted.'));
-
-		return $this->redirect(['action' => 'index', 'library' => 'cms_post']);
-	}
+	use \cms_core\controllers\AdminPublishTrait;
+	use \cms_core\controllers\AdminPromoteTrait;
 }
 
 ?>
