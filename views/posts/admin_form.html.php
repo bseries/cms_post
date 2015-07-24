a5417a27c2179e5d1249f364043d889b372e768a
David Persson <davidpersson@gmx.de>
2015-07-24 16:12:05 +0200

    Remove unused class.

--- views/posts/admin_form.html.php
+++ views/posts/admin_form.html.php
@@ -21,7 +21,7 @@ $this->set([
 ]);
 
 ?>
-<article class="view-<?= $this->_config['controller'] . '-' . $this->_config['template'] ?>">
+<article>
 	<?=$this->form->create($item) ?>
 		<?php if ($useOwner): ?>
 			<div class="grid-row">
